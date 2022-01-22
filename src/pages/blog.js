b66b9db784715a2022c69f86943eed24e7a282c2
Luis Martin Schick <luis3720@hotmail.com>
2022-01-22 14:36:13 +0200

    Added the contents of blogpages to the website via MDX-Renderer

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,22 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import { MDXRenderer} from "gatsby-plugin-mdx";
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Thoughts">
             <ul>
                 {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
+                    data.allMdx.nodes.map(node => (
+                        <article key={node.id}>
+                            <h2>{node.frontmatter.title}</h2>
+                            <p>Posted: {node.frontmatter.date}, Updated: {node.parent.modifiedTime}</p>
+                            <p>By {node.frontmatter.author}</p>
+                            <MDXRenderer>
+                                {node.body}
+                            </MDXRenderer>
+                        </article>
                     ))
                 }
             </ul>
@@ -20,9 +26,20 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
+          author
+        }
+        id
+        body
+        parent {
+          ... on File {
+            modifiedTime(formatString: "MMMM D, YYYY")
+          }
+        }
       }
     }
   }
