3e199d5dfbfdfb3af708f18dfa49794903e512c5
Luis Martin Schick <luis3720@hotmail.com>
2022-01-24 21:23:26 +0200

    Added "gatsby-transformer-sharp"

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
         "gatsby-plugin-image",
         "gatsby-plugin-sharp",
         "gatsby-remark-images",
+        "gatsby-transformer-sharp",
         {
             resolve: "gatsby-source-filesystem",
             options: {
