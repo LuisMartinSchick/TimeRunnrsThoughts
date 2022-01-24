8caf1b0fa7844023a8d599d5a85f00fea46bd540
Luis Martin Schick <luis3720@hotmail.com>
2022-01-24 20:49:07 +0200

    Individual pages for each blog entry

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,21 +1,18 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import { MDXRenderer} from "gatsby-plugin-mdx";
+import {Link, graphql} from 'gatsby'
+import Layout from '../../components/layout'
 
-const BlogPage = ({ data }) => {
+const BlogPage = ({data}) => {
     return (
         <Layout pageTitle="My Thoughts">
             <ul>
                 {
                     data.allMdx.nodes.map(node => (
                         <article key={node.id}>
-                            <h2>{node.frontmatter.title}</h2>
+                            <h2>
+                                <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+                            </h2>
                             <p>Posted: {node.frontmatter.date}, Updated: {node.parent.modifiedTime}</p>
-                            <p>By {node.frontmatter.author}</p>
-                            <MDXRenderer>
-                                {node.body}
-                            </MDXRenderer>
                         </article>
                     ))
                 }
@@ -34,7 +31,7 @@ export const query = graphql`
           author
         }
         id
-        body
+        slug
         parent {
           ... on File {
             modifiedTime(formatString: "MMMM D, YYYY")
